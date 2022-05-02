9878bf8c2022a3e540aa26b921216d308eeb7a34
Sophoan Meas <sophoan.meas@hotmail.com>
2022-05-01 20:03:05 -0400

    fixed template.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const emailValidator = "nodejs-email-validation";
+const emailValidation = require("nodejs-email-validation");
 const cl = require("cli-color");
 
-const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -20,8 +19,9 @@ function error(string) {
   return console.log(cl.redBright.bold(string));
 }
 
-function addManager(){
-    return inquirer
+const addManager = () => {
+  console.log("Start building your team profile");
+  return inquirer
     .prompt([
       {
         type: "input",
@@ -33,44 +33,44 @@ function addManager(){
           } else {
             error("Please enter a valid name!");
           }
-        }
+        },
       },
-      {
-        type: "input",
-        name: "managerId",
-        message: "Enter the manager ID:",
-        validate: (input) => {
-          if (input) {
-            return true;
-          } else {
-            error("Please enter the Manager ID!");
-          }
-        }
-      },
-      {
-        type: "input",
-        name: "managerEmail",
-        message: "Enter (manager) email-address:",
-        validate: (input) => {
-          if (emailValidation.validate(input)) {
-            return true;
-          } else {
-            error(" Please a valid email address!");
-          }
-        }
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "Enter the manager's office number:",
-        validate: (input) => {
-          if (!isNaN(input)) {
-            return true;
-          } else {
-            error("Please enter a valid office number!");
-          }
-        }
-      },
+      //   {
+      //     type: "input",
+      //     name: "managerId",
+      //     message: "Enter the manager ID:",
+      //     validate: (input) => {
+      //       if (input) {
+      //         return true;
+      //       } else {
+      //         error("Please enter the Manager ID!");
+      //       }
+      //     },
+      //   },
+      //   {
+      //     type: "input",
+      //     name: "managerEmail",
+      //     message: "Enter (manager) email-address:",
+      //     validate: (input) => {
+      //       if (emailValidation.validate(input)) {
+      //         return true;
+      //       } else {
+      //         error(" Please a valid email address!");
+      //       }
+      //     },
+      //   },
+      //   {
+      //     type: "input",
+      //     name: "officeNumber",
+      //     message: "Enter the manager's office number:",
+      //     validate: (input) => {
+      //       if (!isNaN(input)) {
+      //         return true;
+      //       } else {
+      //         error("Please enter a valid office number!");
+      //       }
+      //     },
+      //   },
     ])
     .then((ans) => {
       const manager = new Manager(
@@ -81,90 +81,189 @@ function addManager(){
       );
       team.push(manager);
       id.push(ans.managerId);
-      addTeamMembers();
+      addEmployee();
     });
-}
+};
 
-function addEngineer(){
-    return inquirer.prompt([{
-        type: 'input',
-        name: 'engineerName',
+const addEngineer = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
         message: "Enter the engineer's name:",
         validate: (input) => {
+          if (input) {
+            return true;
+          } else {
+            error("Please a valid name!");
+          }
+        },
+      },
+      //   {
+      //     type: "input",
+      //     name: "engineerId",
+      //     message: "Enter the Engineer ID:",
+      //     validate: (input) => {
+      //       if (input) {
+      //         return true;
+      //       } else {
+      //         error("Please enter the Engineer ID!");
+      //       }
+      //     },
+      //   },
+      //   {
+      //     type: "input",
+      //     name: "engineerEmail",
+      //     message: "Enter (engineer) email-address:",
+      //     validate: (input) => {
+      //       if (emailValidation.validate(input)) {
+      //         return true;
+      //       } else {
+      //         error(" Please a valid email address!");
+      //       }
+      //     },
+      //   },
+      //   {
+      //     type: "input",
+      //     name: "engineerGithub",
+      //     message: "Enter (engineer) gitHub username:",
+      //     validate: (input) => {
+      //       if (input) {
+      //         return true;
+      //       } else {
+      //         error("Please enter a valid username!");
+      //       }
+      //     },
+      //   },
+    ])
+    .then((ans) => {
+      const engineer = new Engineer(
+        ans.engineerName,
+        ans.engineerId,
+        ans.engineerEmail,
+        ans.engineerGithub
+      );
+      team.push(engineer);
+      id.push(ans.engineerId);
+      addEmployee();
+    });
+};
+
+const addIntern = () =>{
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "Enter the intern's name:",
+          validate: (input) => {
             if (input) {
               return true;
             } else {
-              error("Please a valid name!");
-            }
-          }
-        },
-          {
-            type:'input',
-            name: 'engineerId',
-            message: 'Enter the Engineer ID:',
-            validate: (input) => {
-                if (input) {
-                  return true;
-                } else {
-                  error("Please enter the Engineer ID!");
-                }
-              }
-          },
-          {
-            type: "input",
-            name: "engineerEmail",
-            message: "Enter (engineer) email-address:",
-            validate: (input) => {
-              if (emailValidation.validate(input)) {
-                return true;
-              } else {
-                error(" Please a valid email address!");
-              }
+              error("Please enter a valid username!");
             }
           },
-          {
-            type: "input",
-            name: "engineerGithub",
-            message: "Enter (engineer) gitHub username:",
-            validate: (input) => {
-              if (emailValidation.validate(input)) {
-                return true;
-              } else {
-                error("Please a valid username!");
-              }
-            }
-          }
-    ]).then(ans =>{
-        const engineer = new Engineer(ans.engineerName, ans.engineerId, ans.engineerEmail, ans.engineerGithub);
-        team.push(engineer);
-        id.push(ans.engineerId);
-        addTeamMembers();
-    })
-}
-
-function addTeamMembers() {
-    return inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "memberType",
-          message: "What member would you like to add?",
-          choices: ["Manager", "Engineer", "Intern", "exit"],
         },
+        // {
+        //   type: "input",
+        //   name: "internId",
+        //   message: "Enter the Intern ID",
+        //   validate: (input) => {
+        //     if (input) {
+        //       return true;
+        //     } else {
+        //       error("Please enter a valid ID!");
+        //     }
+        //   },
+        // },
+        // {
+        //   type: "input",
+        //   name: "internEmail",
+        //   message: "Enter (intern) email-address:",
+        //   validate: (input) => {
+        //     if (emailValidation.validate(input)) {
+        //       return true;
+        //     } else {
+        //       error(" Please a valid email address!");
+        //     }
+        //   },
+        // },
+        // {
+        //   type: "input",
+        //   name: "internSchool",
+        //   message: "Enter (intern) school name:",
+        //   validate: (input) => {
+        //     if (input) {
+        //       return true;
+        //     } else {
+        //       error(" Please a valid school name!");
+        //     }
+        //   },
+        // },
       ])
       .then((ans) => {
-        const selection = ans.memberType;
-        if (selection === "Manager") {
-          addManager();
-        } else if (selection === "Engineer") {
-          addEngineer();
-        } else if (selection === "Intern") {
-          addIntern();
-        }else{
-          generateFile();
-        }
+        const intern = new Intern(
+          ans.internName,
+          ans.internId,
+          ans.internEmail,
+          ans.internSchool
+        );
+        team.push(intern);
+        id.push(ans.internId);
+        addEmployee();
       });
   }
   
 
+const addEmployee = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "memberType",
+        message: "What member would you like to add?",
+        choices: ["Manager", "Engineer", "Intern", "Exit"],
+      },
+    ])
+    .then((ans) => {
+      const selection = ans.memberType;
+      if (selection === "Manager") {
+        addManager();
+      } else if (selection === "Engineer") {
+        addEngineer();
+      } else if (selection === "Intern") {
+        addIntern();
+      } else {
+        generateFile();
+      }
+    });
+};
 
+
+function init() {
+  addManager();
+  // .then(team => {
+  //     return render(team)
+  // })
+}
+
+const generateFile = () => {
+  console.log("Generating Team Profile.... ");
+  // fs.writeFileSync(OUTPUT_PATH, render(team), "utf-8");
+  return new Promise((resolve, reject) => {
+    fs.writeFileSync(OUTPUT_PATH, render(team), (err) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: `${fileName} successfully created! Please check out the dist/index.html"`,
+      });
+    });
+  });
+};
+
+init();
---
+++ src/template.js
@@ -0,0 +1,116 @@
+module.exports = (team) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>Team Profile Generator</title>
+    </head>
+
+    <body>
+    <!-- Title -->
+        <div class="container-fluid">
+            <div class="row">
+            <div class="col py-3 text-center mb-5 title">
+            <h1>Web Dev Team</h1>
+                </div>
+            </div>
+        </div>
+
+        <!-- Employee Container -->
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-12 d-flex justify-content-center employee-list-container">
+                    ${createEmployee(team)}
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+    `;
+};
+
+const createEmployee = (team) => {
+  const addManager = (manager) => {
+    return `
+    <!-- Employee Manager Card -->
+            <div class="col-lg-4 d-flex align-items-stretch card employee-card ">
+    <div class="card-body card-title">
+        <h3 class="card-employee-name">${manager.getName()}</h3>
+        <h4 class="card-employee-role mt-4 icon-manager bi bi-person-workspace">Manager</h4>
+    </div>
+    <div class="card-body bg-light card-attributes">
+        <ul class="list-group text-dark">
+            <li class="list-group-item"><i class="card-icon bi-key"></i>ID: ${manager.getId()}</li>
+            <li class="list-group-item"><i class="card-icon bi-envelope"></i>Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item"><i class="card-icon bi-telephone"></i>Office number: <a href="tel:${manager.getOfficeNumber()}">${manager.getOfficeNumber()}</a></li>
+        </ul>
+        </div>
+    </div>
+        `;
+  };
+
+  const addEngineer = (engineer) => {
+    return `
+    <!-- Employee Engineer Card -->
+            <div class="col-lg-4 d-flex align-items-stretch card employee-card">
+    <div class="card-body card-title">
+        <h3 class="card-employee-name">${engineer.getName()}</h3>
+        <h4 class="card-employee-role mt-4 icon-engineer bi bi-wrench">${engineer.getRole()}</h4>
+    </div>
+    <div class="card-body bg-light card-attributes">
+        <ul class="list-group text-dark">
+            <li class="list-group-item"><i class="card-icon bi-key"></i>ID: ${engineer.getId()}</li>
+            <li class="list-group-item"><i class="card-icon bi-envelope"></i>Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item"><i class="card-icon bi-github"></i>GitHub: <a href="https://github.com/${engineer.getGitHub()}" target="_blank">${engineer.getGitHub()}</a></li>
+        </ul>
+    </div>
+</div>
+        `;
+  };
+
+  const addIntern = (intern) => {
+    return `
+    <!-- Employee Intern Card -->
+            <div class="col-lg-4 d-flex align-items-stretch card employee-card">
+    <div class="card-body card-title">
+        <h3 class="card-employee-name">${intern.getName()}</h3>
+        <h4 class="card-employee-role mt-4 icon-intern bi bi-book-half">${intern.getRole()}</h4>
+    </div>
+    <div class="card-body bg-light card-attributes">
+        <ul class="list-group text-dark">
+            <li class="list-group-item"><i class="card-icon bi-key"></i>ID: ${intern.getId()}</li>
+            <li class="list-group-item"><i class="card-icon bi-envelope"></i>Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item"><i class="card-icon bi-house"></i>School: ${intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+  };
+
+  const profile = [];
+
+  const newManager = team
+    .filter((employee) => employee.getRole() === "Manager")
+    .map((manager) => addManager(manager)).join('');
+  profile.push(newManager);
+  //   console.log(newManager)
+
+  const newEngineer = team
+    .filter((employee) => employee.getRole() === "Engineer")
+    .map((engineer) => addEngineer(engineer)).join('');
+  profile.push(newEngineer);
+  //   console.log(newEngineer);
+
+  const newIntern = team
+    .filter((employee) => employee.getRole() === "Intern")
+    .map((intern) => addIntern(intern)).join('');
+  profile.push(newIntern);
+
+  return profile.join('');
+};
